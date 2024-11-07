bc9980d2f2edc9134f55c26224eb1fc7d64beef5
olegk <kbryzhenyukpc@gmail.com>
2024-11-07 10:36:44 +0200

    Добавьте файлы проекта.

---
+++ Кусгкышщт 1.2/Кусгкышщт 1.2.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+void printStars(int n) {
+    if (n <= 0) return;
+    cout << "*";
+    printStars(n - 1);
+}
+
+int main() {
+    int n;
+    cout << "количество звезд ";
+    cin >> n;
+    printStars(n);
+    cout << endl;
+    return 0;
+}
